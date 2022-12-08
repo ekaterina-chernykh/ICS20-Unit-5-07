5bcff0eedee194871e454109660141c1cb60dab3
ekaterina-chernykh <113055918+ekaterina-chernykh@users.noreply.github.com>
2022-12-07 22:17:46 -0500

    name changes

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS20-PWA-Test/sw.js", {
-    scope: "/ICS20-PWA-Test/",
+  navigator.serviceWorker.register("/ICS20-Unit-5-07/sw.js", {
+    scope: "/ICS20-Unit-5-07/",
   })
 }
 
